9444348c25959317d1923bb9e17c9aae38e9971f
Anton Gorbunov <crayfish921@gmail.com>
2024-05-24 13:19:41 +0300

    -api default base url added

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const apiInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://www.omdbapi.com',
   params: {
     apikey: '6297fc83'
   }
